b6fbb0fe941f00359f16b171ddfbfe37c78d5b48
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-07-17 14:51:35 -0700

    added smooth scrolling

--- scripts.js
+++ scripts.js
@@ -5,4 +5,20 @@ $('.main-carousel').flickity({
   autoPlay: true
 });
 
-document.querySelector(#about).scrollIntoView({ behavior: 'smooth' });
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
