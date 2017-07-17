8f03181428a4cb2c26af170d41dc5df795a212b4
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-07-17 11:47:10 -0700

    more spacing changes

--- scripts.js
+++ scripts.js
@@ -1,18 +1,10 @@
 $('.main-carousel').flickity({
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  autoPlay: true
 });
 
-// var elem = document.querySelector('.main-carousel');
-// var flkty = new Flickity( elem, {
-//   // options
-//   cellAlign: 'left',
-//   contain: true
-// });
-
-// // element argument can be a selector string
-// //   for an individual element
-// var flkty = new Flickity( '.main-carousel', {
-//   // options
+// $('.flickity-page-dots').flickity({
+//   bottom: 13px
 // });
