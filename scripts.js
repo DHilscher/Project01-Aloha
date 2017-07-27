4dc6cd94059992937c65df70595eeb7a56a2c4e1
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-07-27 09:04:02 -0700

    semantics

--- scripts.js
+++ scripts.js
@@ -35,3 +35,10 @@ $(function(){
         }
     });
 });
+
+// Fix "Skip Link" Focus in Webkit
+$(function() {
+   $("a[href^='#']").not("a[href='#']").click(function() {
+      $("#"+$(this).attr("href").slice(1)+"").focus();
+   });
+});
